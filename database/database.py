fe9303a04d90c6057e66b13e6fc4aeb735331b6a
Ulrik.Hansen <uha010@uib.no>
2022-03-05 15:59:23 +0100

    Simple file based database script implemented

---
+++ database/database.py
@@ -0,0 +1,31 @@
+from core import Champion
+from socket import socket, AF_INET, SOCK_DGRAM
+
+
+def _parse_champ(champ_text: str) -> Champion:
+    name, rock, paper, scissors = champ_text.split(sep=',')
+    return Champion(name, float(rock), float(paper), float(scissors))
+
+
+def from_csv(filename: str) -> dict[str, Champion]:
+    champions = {}
+    with open(filename, 'r') as f:
+        for line in f.readlines():
+            champ = _parse_champ(line)
+            champions[champ.name] = champ
+    return champions
+
+
+def load_some_champs():
+    return from_csv('some_champs.txt')
+
+sock = socket(AF_INET,SOCK_DGRAM)
+
+
+sock.bind("",5555)
+
+while True:
+    req, source = sock.recvfrom(64).decode('utf-8')
+    db = load_some_champs()
+    ans = db[req]
+    sock.sendto(ans,source)
