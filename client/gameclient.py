40ec85a197d2751550d84a9e79fc123db1d283fd
Ulrik.Hansen <uha010@uib.no>
2022-03-09 22:28:44 +0100

    Possibly MVP of client

---
+++ client/gameclient.py
@@ -0,0 +1,108 @@
+from socket import socket
+from rich import print
+from rich.prompt import Prompt
+from rich.table import Table
+from core import Champion, Shape, Match, Team
+import pickle as pic
+
+def print_available_champs(champions: dict[Champion]) -> None:
+
+    # Create a table containing available champions
+    available_champs = Table(title='Available champions')
+
+    # Add the columns Name, probability of rock, probability of paper and
+    # probability of scissors
+    available_champs.add_column("Name", style="cyan", no_wrap=True)
+    available_champs.add_column("prob(:raised_fist-emoji:)", justify="center")
+    available_champs.add_column("prob(:raised_hand-emoji:)", justify="center")
+    available_champs.add_column("prob(:victory_hand-emoji:)", justify="center")
+
+    # Populate the table
+    for champion in champions.values():
+        available_champs.add_row(*champion.str_tuple)
+
+    print(available_champs)
+
+def print_match_summary(match: Match) -> None:
+
+    EMOJI = {
+        Shape.ROCK: ':raised_fist-emoji:',
+        Shape.PAPER: ':raised_hand-emoji:',
+        Shape.SCISSORS: ':victory_hand-emoji:'
+    }
+
+    # For each round print a table with the results
+    for index, round in enumerate(match.rounds):
+
+        # Create a table containing the results of the round
+        round_summary = Table(title=f'Round {index+1}')
+
+        # Add columns for each team
+        round_summary.add_column("Red",
+                                 style="red",
+                                 no_wrap=True)
+        round_summary.add_column("Blue",
+                                 style="blue",
+                                 no_wrap=True)
+
+        # Populate the table
+        for key in round:
+            red, blue = key.split(', ')
+            round_summary.add_row(f'{red} {EMOJI[round[key].red]}',
+                                  f'{blue} {EMOJI[round[key].blue]}')
+        print(round_summary)
+        print('\n')
+
+    # Print the score
+    red_score, blue_score = match.score
+    print(f'Red: {red_score}\n'
+          f'Blue: {blue_score}')
+
+    # Print the winner
+    if red_score > blue_score:
+        print('\n[red]Red victory! :grin:')
+    elif red_score < blue_score:
+        print('\n[blue]Blue victory! :grin:')
+    else:
+        print('\nDraw :expressionless:')
+
+def input_champ(sock: socket):
+    prompt = sock.recv(1024).decode()
+    
+    if prompt == "Pick a champion: ":
+        while 1:
+            choice = input(prompt)
+            sock.send(choice.encode())
+            success = pic.load(sock.recv(32))
+            print(sock.recv(1024).decode())
+            if success:
+                break
+    else:
+        print(sock.recv(1024).decode())
+        
+
+
+
+sock = socket()
+sock.connect(("tntserver",5555))
+
+print(sock.recv(1024).decode())
+
+print(sock.recv(1024).decode())
+
+champions = pic.load(sock.recv(4096))
+
+print_available_champs(champions)
+print("\n")
+
+for _ in range(4):
+    input_champ(sock)
+
+print("\n")
+
+match = pic.load(sock.recv(2048))
+print_match_summary(match)
+
+print("\n\nGG")
+
+sock.close()
